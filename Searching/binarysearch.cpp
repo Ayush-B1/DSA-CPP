79fdffa778299249a1995f8da9486e766b5e71ad
Ayush-B1 <ayush.scb@gmail.com>
2025-07-23 12:22:36 +0530

    Second Commit

--- Searching/binarysearch.cpp
+++ Searching/binarysearch.cpp
@@ -6,10 +6,36 @@ int binarySearch(int *arr, int n, int key){
     int low = 0;
     int high = n-1;
 
-    
+
+    while(low <= high){
+        int mid = low + (high - low)/2;
+        if(arr[mid] == key){
+            return mid;
+        }
+        else if(arr[mid] > key){
+            high = mid - 1;
+        }else{
+            low = mid + 1;
+        }
+    }
+
+    return -1;
+
 
 }
 
 int main(){
-    
+    int arr[5] = {1, 2, 3, 4, 5};
+
+    int n = sizeof(arr)/sizeof(int);
+    int key = 5;
+
+    int result = binarySearch(arr, n, key);
+
+    if(result != -1){
+        cout<<"The element is present in: "<<result<<" index"<<endl;
+    }else{
+        cout<<"The element is not present in the array"<<endl;
+    }
+
 }
